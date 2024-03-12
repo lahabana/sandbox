a3540901d357e43cbe00cd64627abb5a3195b2cf
Charly Molter <charly.molter@konghq.com>
2024-03-12 13:24:35 +0100

    can't even do this
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- test.go
+++ test.go
@@ -8,7 +8,7 @@ func main() {
 	println(runtime.NumCPU())
 
 	println("Hello")
-	println("::group my info")
+	println("::group:: my info")
 	println("Some cool text")
 	println("Evan more data")
 	println("::endgroup::")
