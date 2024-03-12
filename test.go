3379a129f66e91318d9d754c3887ba8a5052f4a3
Charly Molter <charly.molter@konghq.com>
2024-03-12 13:23:00 +0100

    experiment with group
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- test.go
+++ test.go
@@ -6,4 +6,10 @@ import (
 
 func main() {
 	println(runtime.NumCPU())
+
+	println("Hello")
+	println("::group my info")
+	println("Some cool text")
+	println("Evan more data")
+	println("::endgroup::")
 }
